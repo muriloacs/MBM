78e76c92cd566a11b90efbed862e177d79090d93
Murilo Santiago <murilo.santiago@fs.com.br>
2016-03-09 15:34:16 -0300

    configs updated

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -2,15 +2,16 @@
 /**
  * Doctrine's database connection for production enviroment.
  */
+
 return array(
-  'doctrine' => array(
-    'connection' => array(
-        'orm_default' => array(
-          'driverClass' =>'Doctrine\DBAL\Driver\PDOMySql\Driver',
-          'params' => array(
-            'host'     => 'myhost',
-            'port'     => '3306',
-            'user'     => 'myuser',
-            'password' => 'mypassword',
-            'dbname'   => 'mydbname',
+    'doctrine' => array(
+        'connection' => array(
+            'orm_default' => array(
+                'driverClass' =>'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host'     => 'localhost',
+                    'port'     => '3306',
+                    'user'     => 'root',
+                    'password' => 'muk10502',
+                    'dbname'   => 'mbm',
 )))));
